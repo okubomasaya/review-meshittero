e68503928fa8136e3021c9765fdb65f9a4da044b
okubomasaya <ooookubo.48@gmail.com>
2021-09-09 12:50:08 +0000

    save

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'af6d494b60ffc7f857b93ba4ba832200afa8bcb4171ae64f2fd12fab994519ddba9a518a9819bf13d6bfe5067bf8b6a824e7de0608f1da8a618ecd22b8db0acd'
